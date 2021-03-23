cfee9f8d1b75a40ab67bd8774dcd196a8914c6d2
vroyer <vroyer@strapdata.com>
2021-03-24 00:03:22 +0100

    Remove quasar

--- quasar-consumer/src/main/java/com/datastax/cassandra/cdc/consumer/exceptions/ServiceNotRunningException.java
+++
@@ -1,9 +0,0 @@
-package com.datastax.cassandra.cdc.consumer.exceptions;
-
-import com.datastax.cassandra.cdc.quasar.State;
-
-public class ServiceNotRunningException extends QuasarConsumerException {
-    public ServiceNotRunningException(State state) {
-        super(state, "status="+state.getStatus());
-    }
-}
