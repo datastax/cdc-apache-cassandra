f5e108a5f153183a716bd4398d77928722fae9ca
Vincent Royer <vincent.royer@datastax.com>
2021-05-21 15:30:19 +0200

    Add a prefix for config system properties

--- producer/src/main/java/com/datastax/cassandra/cdc/producer/ProducerConfig.java
+++ producer/src/main/java/com/datastax/cassandra/cdc/producer/ProducerConfig.java
@@ -26,6 +26,7 @@ import java.util.function.Supplier;
 @Slf4j
 public class ProducerConfig {
 
+    public static final String CDC_PROPERTY_PREFIX = "cdc.";
     public static final String storageDir = System.getProperty("cassandra.storagedir", null);
 
     enum Plateform {
@@ -41,107 +42,107 @@ public class ProducerConfig {
     }
 
     public static final String CDC_RELOCATION_DIR = "cdcRelocationDir";
-    public static String cdcRelocationDir = System.getProperty(CDC_RELOCATION_DIR, storageDir + File.separator + "cdc_backup");
+    public static String cdcRelocationDir = System.getProperty(CDC_PROPERTY_PREFIX + CDC_RELOCATION_DIR, storageDir + File.separator + "cdc_backup");
     public static final Setting<String> CDC_RELOCATION_DIR_SETTING =
             new Setting<>(CDC_RELOCATION_DIR, Plateform.ALL, s -> cdcRelocationDir = s, () -> cdcRelocationDir);
 
     public static final String CDC_DIR_POOL_INTERVAL_MS = "cdcPoolIntervalMs";
-    public static long cdcDirPollIntervalMs = Long.getLong(CDC_DIR_POOL_INTERVAL_MS, 60000L);
+    public static long cdcDirPollIntervalMs = Long.getLong(CDC_PROPERTY_PREFIX + CDC_DIR_POOL_INTERVAL_MS, 60000L);
     public static final Setting<Long> CDC_DIR_POOL_INTERVAL_MS_SETTING =
             new Setting<>(CDC_DIR_POOL_INTERVAL_MS, Plateform.ALL, s -> cdcDirPollIntervalMs = Long.parseLong(s), () -> cdcDirPollIntervalMs);
 
     public static final String ERROR_COMMITLOG_REPROCESS_ENABLED = "errorCommitLogReprocessEnabled";
-    public static boolean errorCommitLogReprocessEnabled = Boolean.getBoolean(ERROR_COMMITLOG_REPROCESS_ENABLED);
+    public static boolean errorCommitLogReprocessEnabled = Boolean.getBoolean(CDC_PROPERTY_PREFIX + ERROR_COMMITLOG_REPROCESS_ENABLED);
     public static final Setting<Boolean> ERROR_COMMITLOG_REPROCESS_ENABLED_SETTING =
             new Setting<>(ERROR_COMMITLOG_REPROCESS_ENABLED, Plateform.ALL, s -> errorCommitLogReprocessEnabled = Boolean.parseBoolean(s), () -> errorCommitLogReprocessEnabled);
 
     public static final String EMIT_TOMBSTONE_ON_DELETE = "emitTombstoneOnDelete";
-    public static boolean emitTombstoneOnDelete = Boolean.getBoolean(EMIT_TOMBSTONE_ON_DELETE);
+    public static boolean emitTombstoneOnDelete = Boolean.getBoolean(CDC_PROPERTY_PREFIX + EMIT_TOMBSTONE_ON_DELETE);
     public static final Setting<Boolean> EMIT_TOMBSTONE_ON_DELETE_SETTING =
             new Setting<>(EMIT_TOMBSTONE_ON_DELETE, Plateform.ALL, s -> emitTombstoneOnDelete = Boolean.parseBoolean(s), () -> emitTombstoneOnDelete);
 
     public static final String TOPIC_PREFIX = "topicPrefix";
-    public static String topicPrefix = System.getProperty(TOPIC_PREFIX, "events-");
+    public static String topicPrefix = System.getProperty(CDC_PROPERTY_PREFIX + TOPIC_PREFIX, "events-");
     public static final Setting<String> TOPIC_PREFIX_SETTING =
             new Setting<>(TOPIC_PREFIX, Plateform.ALL, s -> topicPrefix = s, () -> topicPrefix);
 
     public static final String PULSAR_SERVICE_URL = "pulsarServiceUrl";
-    public static String pulsarServiceUrl = System.getProperty(PULSAR_SERVICE_URL, "pulsar://localhost:6650");
+    public static String pulsarServiceUrl = System.getProperty(CDC_PROPERTY_PREFIX + PULSAR_SERVICE_URL, "pulsar://localhost:6650");
     public static final Setting<String> PULSAR_SERVICE_URL_SETTING =
             new Setting<>(PULSAR_SERVICE_URL, Plateform.PULSAR, s -> pulsarServiceUrl = s, () -> pulsarServiceUrl);
 
     public static final String KAFKA_BROKERS = "kafkaBrokers";
-    public static String kafkaBrokers = System.getProperty(KAFKA_BROKERS, "localhost:9092");
+    public static String kafkaBrokers = System.getProperty(CDC_PROPERTY_PREFIX + KAFKA_BROKERS, "localhost:9092");
     public static final Setting<String> KAFKA_BROKERS_SETTING =
             new Setting<>(KAFKA_BROKERS, Plateform.KAFKA, s -> kafkaBrokers = s, () -> kafkaBrokers);
 
     public static final String KAFKA_SCHEMA_REGISTRY_URL = "kafkaSchemaRegistryUrl";
-    public static String kafkaSchemaRegistryUrl = System.getProperty(KAFKA_SCHEMA_REGISTRY_URL, "http://localhost:8081");
+    public static String kafkaSchemaRegistryUrl = System.getProperty(CDC_PROPERTY_PREFIX + KAFKA_SCHEMA_REGISTRY_URL, "http://localhost:8081");
     public static final Setting<String> KAFKA_SCHEMA_REGISTRY_URL_SETTING =
             new Setting<>(KAFKA_SCHEMA_REGISTRY_URL, Plateform.KAFKA, s -> kafkaSchemaRegistryUrl = s, () -> kafkaSchemaRegistryUrl);
 
     public static final String SSL_PROVIDER = "sslProvider";
-    public static String sslProvider = System.getProperty(SSL_PROVIDER);
+    public static String sslProvider = System.getProperty(CDC_PROPERTY_PREFIX + SSL_PROVIDER);
     public static final Setting<String> SSL_PROVIDER_SETTING =
             new Setting<>(SSL_PROVIDER, Plateform.ALL, s -> sslProvider = s, () -> sslProvider);
 
     public static final String SSL_TRUSTSTORE_PATH = "sslTruststorePath";
-    public static String sslTruststorePath = System.getProperty(SSL_TRUSTSTORE_PATH);
+    public static String sslTruststorePath = System.getProperty(CDC_PROPERTY_PREFIX + SSL_TRUSTSTORE_PATH);
     public static final Setting<String> SSL_TRUSTSTORE_PATH_SETTING =
             new Setting<>(SSL_TRUSTSTORE_PATH, Plateform.ALL, s -> sslTruststorePath = s, () -> sslTruststorePath);
 
     public static final String SSL_TRUSTSTORE_PASSWORD = "sslTruststorePassword";
-    public static String sslTruststorePassword = System.getProperty(SSL_TRUSTSTORE_PASSWORD);
+    public static String sslTruststorePassword = System.getProperty(CDC_PROPERTY_PREFIX + SSL_TRUSTSTORE_PASSWORD);
     public static final Setting<String> SSL_TRUSTSTORE_PASSWORD_SETTING =
             new Setting<>(SSL_TRUSTSTORE_PASSWORD, Plateform.ALL, s -> sslTruststorePassword = s, () -> sslTruststorePassword);
 
     public static final String SSL_TRUSTSTORE_TYPE = "sslTruststoreType";
-    public static String sslTruststoreType = System.getProperty(SSL_TRUSTSTORE_TYPE, "JKS");
+    public static String sslTruststoreType = System.getProperty(CDC_PROPERTY_PREFIX + SSL_TRUSTSTORE_TYPE, "JKS");
     public static final Setting<String> SSL_TRUSTSTORE_TYPE_SETTING =
             new Setting<>(SSL_TRUSTSTORE_TYPE, Plateform.ALL, s -> sslTruststoreType = s, () -> sslTruststoreType);
 
     public static final String SSL_KEYSTORE_PATH = "sslKeystorePath";
-    public static String sslKeystorePath = System.getProperty(SSL_KEYSTORE_PATH);
+    public static String sslKeystorePath = System.getProperty(CDC_PROPERTY_PREFIX + SSL_KEYSTORE_PATH);
     public static final Setting<String> SSL_KEYSTORE_PATH_SETTING =
             new Setting<>(SSL_KEYSTORE_PATH, Plateform.ALL, s -> sslKeystorePath = s, () -> sslKeystorePath);
 
     public static final String SSL_KEYSTORE_PASSWORD = "sslKeystorePassword";
-    public static String sslKeystorePassword = System.getProperty(SSL_KEYSTORE_PASSWORD);
+    public static String sslKeystorePassword = System.getProperty(CDC_PROPERTY_PREFIX + SSL_KEYSTORE_PASSWORD);
     public static final Setting<String> SSL_KEYSTORE_PASSWORD_SETTING =
             new Setting<>(SSL_KEYSTORE_PASSWORD, Plateform.ALL, s -> sslKeystorePassword = s, () -> sslKeystorePassword);
 
     public static final String SSL_CIPHER_SUITES = "sslCipherSuites";
-    public static String sslCipherSuites = System.getProperty(SSL_CIPHER_SUITES);
+    public static String sslCipherSuites = System.getProperty(CDC_PROPERTY_PREFIX + SSL_CIPHER_SUITES);
     public static final Setting<String> SSL_CIPHER_SUITES_SETTING =
             new Setting<>(SSL_CIPHER_SUITES, Plateform.ALL, s -> sslCipherSuites = s, () -> sslCipherSuites);
 
     public static final String SSL_ENABLED_PROTOCOLS = "sslEnabledProtocols";
-    public static String sslEnabledProtocols = System.getProperty(SSL_ENABLED_PROTOCOLS, "TLSv1.2,TLSv1.1,TLSv1");
+    public static String sslEnabledProtocols = System.getProperty(CDC_PROPERTY_PREFIX + SSL_ENABLED_PROTOCOLS, "TLSv1.2,TLSv1.1,TLSv1");
     public static final Setting<String> SSL_ENABLED_PROTOCOLS_SETTING =
             new Setting<>(SSL_ENABLED_PROTOCOLS, Plateform.ALL, s -> sslEnabledProtocols = s, () -> sslEnabledProtocols);
 
     public static final String SSL_ENDPOINT_IDENTIFICATION_ALGORITHM = "sslEndpointIdentificationAlgorithm";
-    public static String sslEndpointIdentificationAlgorithm = System.getProperty(SSL_ENDPOINT_IDENTIFICATION_ALGORITHM, "https");
+    public static String sslEndpointIdentificationAlgorithm = System.getProperty(CDC_PROPERTY_PREFIX + SSL_ENDPOINT_IDENTIFICATION_ALGORITHM, "https");
     public static final Setting<String> SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_SETTING =
             new Setting<>(SSL_ENDPOINT_IDENTIFICATION_ALGORITHM, Plateform.KAFKA, s -> sslEndpointIdentificationAlgorithm = s, () -> sslEndpointIdentificationAlgorithm);
 
     public static final String SSL_ALLOW_INSECURE_CONNECTION = "sslAllowInsecureConnection";
-    public static boolean sslAllowInsecureConnection = Boolean.getBoolean(SSL_ALLOW_INSECURE_CONNECTION);
+    public static boolean sslAllowInsecureConnection = Boolean.getBoolean(CDC_PROPERTY_PREFIX + SSL_ALLOW_INSECURE_CONNECTION);
     public static final Setting<Boolean> SSL_ALLOW_INSECURE_CONNECTION_SETTING =
             new Setting<>(SSL_ALLOW_INSECURE_CONNECTION, Plateform.PULSAR, s -> sslAllowInsecureConnection = Boolean.parseBoolean(s), () -> sslAllowInsecureConnection);
 
     public static final String SSL_HOSTNAME_VERIFICATION_ENABLE = "sslHostnameVerificationEnable";
-    public static boolean sslHostnameVerificationEnable = Boolean.getBoolean(SSL_HOSTNAME_VERIFICATION_ENABLE);
+    public static boolean sslHostnameVerificationEnable = Boolean.getBoolean(CDC_PROPERTY_PREFIX + SSL_HOSTNAME_VERIFICATION_ENABLE);
     public static final Setting<Boolean> SSL_HOSTNAME_VERIFICATION_ENABLE_SETTING =
             new Setting<>(SSL_HOSTNAME_VERIFICATION_ENABLE, Plateform.PULSAR, s -> sslHostnameVerificationEnable = Boolean.parseBoolean(s), () -> sslHostnameVerificationEnable);
 
     public static final String PULSAR_AUTH_PLUGIN_CLASS_NAME = "pulsarAuthPluginClassName";
-    public static String pulsarAuthPluginClassName = System.getProperty(PULSAR_AUTH_PLUGIN_CLASS_NAME);
+    public static String pulsarAuthPluginClassName = System.getProperty(CDC_PROPERTY_PREFIX + PULSAR_AUTH_PLUGIN_CLASS_NAME);
     public static final Setting<String> PULSAR_AUTH_PLUGIN_CLASS_NAME_SETTING =
             new Setting<>(PULSAR_AUTH_PLUGIN_CLASS_NAME, Plateform.PULSAR, s -> pulsarAuthPluginClassName = s, () -> pulsarAuthPluginClassName);
 
     public static final String PULSAR_AUTH_PARAMS = "pulsarAuthParams";
-    public static String pulsarAuthParams = System.getProperty(PULSAR_AUTH_PARAMS);
+    public static String pulsarAuthParams = System.getProperty(CDC_PROPERTY_PREFIX + PULSAR_AUTH_PARAMS);
     public static final Setting<String> PULSAR_AUTH_PARAMS_SETTING =
             new Setting<>(PULSAR_AUTH_PARAMS, Plateform.PULSAR, s -> pulsarAuthParams = s, () -> pulsarAuthParams);
 
