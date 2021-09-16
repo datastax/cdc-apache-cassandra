db64b79b760559d5bb5e1d441ed6323d59968d32
Vincent Royer <vincent.royer@datastax.com>
2021-09-16 14:20:28 +0200

    Remove unless integ-test

--- integ-test/destroy-cluster.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-source integ-test/test-lib.sh
-destroy_cluster
--- integ-test/install_cass_operator.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-source integ-test/test-lib.sh
-setup_flavor
-install_cass_operator
-sleep 5
