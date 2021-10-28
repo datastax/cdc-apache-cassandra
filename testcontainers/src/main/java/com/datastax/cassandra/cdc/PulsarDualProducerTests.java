ea2f4160caf8489ad821eafb9ba3e7b401cf2e97
Vincent Royer <vincent.royer@datastax.com>
2021-10-28 09:18:00 +0200

    Fixup testUnorderedMutations

--- testcontainers/src/main/java/com/datastax/cassandra/cdc/PulsarDualProducerTests.java
+++ testcontainers/src/main/java/com/datastax/cassandra/cdc/PulsarDualProducerTests.java
@@ -304,8 +304,8 @@ public abstract class PulsarDualProducerTests {
                 UUID node2 = nodesPerPk.get("1").get(1);
                 Assert.assertNotEquals(node1, node2);
 
-                // not always in a different order, may depend on system scheduling
-                Assert.assertNotEquals(digestPerNode.get(node1), digestPerNode.get(node2));
+                // not always in a different order, may depend on the system scheduling
+                //Assert.assertNotEquals(digestPerNode.get(node1), digestPerNode.get(node2));
             }
         }
     }
