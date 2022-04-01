85ba48d2d07bccc4b12838be9383167c3f96770f
Enrico Olivelli <eolivelli@gmail.com>
2022-04-01 13:19:08 +0200

    Adapt tests

--- connector-luna/src/test/java/com/datastax/oss/pulsar/source/PulsarCassandraSourceTests.java
+++ connector-luna/src/test/java/com/datastax/oss/pulsar/source/PulsarCassandraSourceTests.java
@@ -31,6 +31,11 @@ import com.datastax.testcontainers.ChaosNetworkContainer;
 import com.datastax.testcontainers.PulsarContainer;
 import com.datastax.testcontainers.cassandra.CassandraContainer;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.shade.org.apache.avro.SchemaBuilder;
+import org.apache.pulsar.shade.org.apache.avro.Conversion;
+import org.apache.pulsar.shade.org.apache.avro.LogicalType;
+import org.apache.pulsar.shade.org.apache.avro.generic.IndexedRecord;
+import org.apache.pulsar.shade.org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.commons.io.IOUtils;
 import org.apache.pulsar.client.api.*;
 import org.apache.pulsar.client.api.schema.Field;
@@ -38,6 +43,10 @@ import org.apache.pulsar.client.api.schema.GenericObject;
 import org.apache.pulsar.client.api.schema.GenericRecord;
 import org.apache.pulsar.common.schema.KeyValue;
 import org.apache.pulsar.common.schema.SchemaType;
+import org.apache.pulsar.shade.org.apache.avro.generic.GenericArray;
+import org.apache.pulsar.shade.org.apache.avro.generic.GenericData;
+import org.apache.pulsar.shade.org.apache.avro.util.Utf8;
+import org.apache.pulsar.shade.org.apache.avro.Schema;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -63,6 +72,11 @@ import static com.datastax.oss.cdc.DataSpec.dataSpecMap;
 
 @Slf4j
 public class PulsarCassandraSourceTests {
+
+    private static final String CQL_VARINT = "cql_varint";
+    private static final String CQL_DECIMAL = "cql_decimal";
+    private static final String CQL_DURATION = "cql_duration";
+
     public static final DockerImageName CASSANDRA_IMAGE = DockerImageName.parse(
             Optional.ofNullable(System.getenv("CASSANDRA_IMAGE"))
                     .orElse("cassandra:" + System.getProperty("cassandraVersion"))
@@ -534,7 +548,7 @@ public class PulsarCassandraSourceTests {
         }
     }
 
-    void assertGenericMap(String field, Map<org.apache.avro.util.Utf8, Object> gm) {
+    void assertGenericMap(String field, Map<Utf8, Object> gm) {
         switch (field) {
             case "map":
                 log.debug("field={} gm={}", field, gm);
@@ -549,7 +563,7 @@ public class PulsarCassandraSourceTests {
         Assert.assertTrue("Unexpected field="+field, false);
     }
 
-    void assertGenericArray(String field, org.apache.avro.generic.GenericArray ga) {
+    void assertGenericArray(String field, GenericArray ga) {
         switch (field) {
             case "set": {
                 Set set = (Set) dataSpecMap.get("set").avroValue;
@@ -568,7 +582,7 @@ public class PulsarCassandraSourceTests {
                 List list = (List) dataSpecMap.get("listofmap").avroValue;
                 for (int i = 0; i < ga.size(); i++) {
                     Map<String, Object> expectedMap = (Map<String, Object>) list.get(i);
-                    Map<org.apache.avro.util.Utf8, Object> gm = (Map<org.apache.avro.util.Utf8, Object>) ga.get(i);
+                    Map<Utf8, Object> gm = (Map<Utf8, Object>) ga.get(i);
                     Assert.assertEquals(expectedMap.size(), gm.size());
                     // convert AVRO Utf8 keys to String.
                     Map<String, Object> actualMap = gm.entrySet().stream().collect(Collectors.toMap(
@@ -581,8 +595,8 @@ public class PulsarCassandraSourceTests {
             }
             case "setofudt": {
                 for (int i = 0; i < ga.size(); i++) {
-                    org.apache.avro.generic.GenericData.Record gr = (org.apache.avro.generic.GenericData.Record) ga.get(i);
-                    for(org.apache.avro.Schema.Field f : gr.getSchema().getFields()) {
+                    GenericData.Record gr = (GenericData.Record) ga.get(i);
+                    for(Schema.Field f : gr.getSchema().getFields()) {
                         assertField(f.name(), gr.get(f.name()));
                     }
                 }
@@ -600,14 +614,14 @@ public class PulsarCassandraSourceTests {
         if (value instanceof GenericRecord) {
             assertGenericRecords(vKey, (GenericRecord) value);
         } else if (value instanceof Collection) {
-            assertGenericArray(vKey, (org.apache.avro.generic.GenericArray) value);
+            assertGenericArray(vKey, (GenericData.Array) value);
         } else if (value instanceof Map) {
             assertGenericMap(vKey, (Map) value);
-        } else if (value instanceof org.apache.avro.util.Utf8) {
+        } else if (value instanceof Utf8) {
             // convert Utf8 to String
             Assert.assertEquals("Wrong value for regular field " + fieldName, dataSpecMap.get(vKey).avroValue, value.toString());
-        } else if (value instanceof org.apache.avro.generic.GenericData.Record) {
-            org.apache.avro.generic.GenericData.Record gr = (org.apache.avro.generic.GenericData.Record) value;
+        } else if (value instanceof GenericData.Record) {
+            GenericData.Record gr = (GenericData.Record) value;
             if (CqlLogicalTypes.CQL_DECIMAL.equals(gr.getSchema().getName())) {
                 CqlLogicalTypes.CqlDecimalConversion cqlDecimalConversion = new CqlLogicalTypes.CqlDecimalConversion();
                 value = cqlDecimalConversion.fromRecord(gr, gr.getSchema(), CqlLogicalTypes.CQL_DECIMAL_LOGICAL_TYPE);
@@ -615,7 +629,7 @@ public class PulsarCassandraSourceTests {
                 CqlLogicalTypes.CqlVarintConversion cqlVarintConversion = new CqlLogicalTypes.CqlVarintConversion();
                 value = cqlVarintConversion.fromRecord(gr, gr.getSchema(), CqlLogicalTypes.CQL_VARINT_LOGICAL_TYPE);
             } else if (CqlLogicalTypes.CQL_DURATION.equals(gr.getSchema().getName())) {
-                NativeAvroConverter.CqlDurationConversion  cqlDurationConversion = new NativeAvroConverter.CqlDurationConversion();
+                CqlLogicalTypes.CqlDurationConversion  cqlDurationConversion = new CqlLogicalTypes.CqlDurationConversion();
                 value = cqlDurationConversion.fromRecord(gr, gr.getSchema(), CqlLogicalTypes.CQL_DURATION_LOGICAL_TYPE);
             }
             Assert.assertEquals("Wrong value for regular field " + fieldName, dataSpecMap.get(vKey).avroValue, value);
@@ -889,6 +903,163 @@ public class PulsarCassandraSourceTests {
             log.info("Function {} logs {}", name, logs);
         } catch (Throwable err) {
             log.info("Cannot download {} logs", name, err);
+        }
+    }
+
+    private static class CqlLogicalTypes {
+
+        public static final String CQL_VARINT = "cql_varint";
+        public static final CqlVarintLogicalType CQL_VARINT_LOGICAL_TYPE = new CqlVarintLogicalType();
+        public static final Schema varintType  = CQL_VARINT_LOGICAL_TYPE.addToSchema(Schema.create(Schema.Type.BYTES));
+
+        public static final String CQL_DECIMAL = "cql_decimal";
+        public static final String CQL_DECIMAL_BIGINT = "bigint";
+        public static final String CQL_DECIMAL_SCALE = "scale";
+        public static final CqlDecimalLogicalType CQL_DECIMAL_LOGICAL_TYPE = new CqlLogicalTypes.CqlDecimalLogicalType();
+        public static final Schema decimalType  = CQL_DECIMAL_LOGICAL_TYPE.addToSchema(
+                SchemaBuilder.record(CQL_DECIMAL)
+                        .fields()
+                        .name(CQL_DECIMAL_BIGINT).type().bytesType().noDefault()
+                        .name(CQL_DECIMAL_SCALE).type().intType().noDefault()
+                        .endRecord()
+        );
+
+        public static final String CQL_DURATION = "cql_duration";
+        public static final String CQL_DURATION_MONTHS = "months";
+        public static final String CQL_DURATION_DAYS = "days";
+        public static final String CQL_DURATION_NANOSECONDS = "nanoseconds";
+        public static final CqlDurationLogicalType CQL_DURATION_LOGICAL_TYPE = new CqlDurationLogicalType();
+        public static final Schema durationType  = CQL_DURATION_LOGICAL_TYPE.addToSchema(
+                SchemaBuilder.record(CQL_DURATION)
+                        .fields()
+                        .name(CQL_DURATION_MONTHS).type().intType().noDefault()
+                        .name(CQL_DURATION_DAYS).type().intType().noDefault()
+                        .name(CQL_DURATION_NANOSECONDS).type().longType().noDefault()
+                        .endRecord()
+        );
+
+        private static class CqlDurationLogicalType extends LogicalType {
+            public CqlDurationLogicalType() {
+                super(CQL_DURATION);
+            }
+
+            @Override
+            public void validate(Schema schema) {
+                super.validate(schema);
+                // validate the type
+                if (schema.getType() != Schema.Type.RECORD) {
+                    throw new IllegalArgumentException("Logical type cql_duration must be backed by a record");
+                }
+            }
+        }
+
+        public static class CqlVarintLogicalType extends LogicalType {
+            public CqlVarintLogicalType() {
+                super(CQL_VARINT);
+            }
+
+            @Override
+            public void validate(Schema schema) {
+                super.validate(schema);
+                // validate the type
+                if (schema.getType() != Schema.Type.BYTES) {
+                    throw new IllegalArgumentException("Logical type cql_varint must be backed by bytes");
+                }
+            }
+        }
+
+        public static class CqlDecimalLogicalType extends LogicalType {
+            public CqlDecimalLogicalType() {
+                super(CQL_DECIMAL);
+            }
+
+            @Override
+            public void validate(Schema schema) {
+                super.validate(schema);
+                // validate the type
+                if (schema.getType() != Schema.Type.RECORD) {
+                    throw new IllegalArgumentException("Logical type cql_decimal must be backed by a record");
+                }
+            }
+        }
+
+        private static class CqlVarintConversion extends Conversion<BigInteger> {
+            @Override
+            public Class<BigInteger> getConvertedType() {
+                return BigInteger.class;
+            }
+
+            @Override
+            public String getLogicalTypeName() {
+                return CQL_VARINT;
+            }
+
+            @Override
+            public BigInteger fromBytes(ByteBuffer value, Schema schema, LogicalType type) {
+                byte[] arr = new byte[value.remaining()];
+                value.duplicate().get(arr);
+                return new BigInteger(arr);
+            }
+
+            @Override
+            public ByteBuffer toBytes(BigInteger value, Schema schema, LogicalType type) {
+                return ByteBuffer.wrap(value.toByteArray());
+            }
+        }
+
+        private static class CqlDecimalConversion extends Conversion<BigDecimal> {
+            @Override
+            public Class<BigDecimal> getConvertedType() {
+                return BigDecimal.class;
+            }
+
+            @Override
+            public String getLogicalTypeName() {
+                return CQL_DECIMAL;
+            }
+
+            @Override
+            public BigDecimal fromRecord(IndexedRecord value, Schema schema, LogicalType type) {
+                ByteBuffer bb = (ByteBuffer) value.get(0);
+                byte[] bytes = new byte[bb.remaining()];
+                bb.duplicate().get(bytes);
+                int scale = (int) value.get(1);
+                return new BigDecimal(new BigInteger(bytes), scale);
+            }
+
+            @Override
+            public IndexedRecord toRecord(BigDecimal value, Schema schema, LogicalType type) {
+                return new GenericRecordBuilder(decimalType)
+                        .set(CQL_DECIMAL_BIGINT, ByteBuffer.wrap(value.unscaledValue().toByteArray()))
+                        .set(CQL_DECIMAL_SCALE, value.scale())
+                        .build();
+            }
+        }
+
+        public static class CqlDurationConversion extends Conversion<CqlDuration> {
+            @Override
+            public Class<CqlDuration> getConvertedType() {
+                return CqlDuration.class;
+            }
+
+            @Override
+            public String getLogicalTypeName() {
+                return CQL_DURATION;
+            }
+
+            @Override
+            public CqlDuration fromRecord(IndexedRecord value, Schema schema, LogicalType type) {
+                return CqlDuration.newInstance((int)value.get(0), (int)value.get(1), (long)value.get(2));
+            }
+
+            @Override
+            public IndexedRecord toRecord(CqlDuration value, Schema schema, LogicalType type) {
+                org.apache.pulsar.shade.org.apache.avro.generic.GenericRecord record = new GenericData.Record(durationType);
+                record.put(CQL_DURATION_MONTHS, value.getMonths());
+                record.put(CQL_DURATION_DAYS, value.getDays());
+                record.put(CQL_DURATION_NANOSECONDS, value.getNanoseconds());
+                return record;
+            }
         }
     }
 }
