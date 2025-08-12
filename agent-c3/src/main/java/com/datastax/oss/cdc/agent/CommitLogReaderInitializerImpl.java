fd0259ce806c8bdfb3fa7c0d22e8d93359dfeeeb
Arkadip <in2arkadipb13@gmail.com>
2025-08-12 17:52:35 +0530

    feat: implement `CommitLogReaderInitializerImpl` for agent initialization across multiple versions

---
+++ agent-c3/src/main/java/com/datastax/oss/cdc/agent/CommitLogReaderInitializerImpl.java
@@ -0,0 +1,8 @@
+package com.datastax.oss.cdc.agent;
+
+public class CommitLogReaderInitializerImpl implements CommitLogReaderInitializer {
+    @Override
+    public void initialize(AgentConfig config, CommitLogReaderService commitLogReaderService) throws Exception {
+
+    }
+}
---
+++ agent-c4/src/main/java/com/datastax/oss/cdc/agent/CommitLogReaderInitializerImpl.java
@@ -0,0 +1,32 @@
+package com.datastax.oss.cdc.agent;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.datastax.oss.cdc.agent.CommitLogReaderService.ARCHIVE_FOLDER;
+import static com.datastax.oss.cdc.agent.CommitLogReaderService.ERROR_FOLDER;
+
+public class CommitLogReaderInitializerImpl implements CommitLogReaderInitializer {
+    @Override
+    public void initialize(AgentConfig config, CommitLogReaderService commitLogReaderService) throws Exception {
+        File relocationDir = new File(config.cdcWorkingDir);
+        if (!relocationDir.exists()) {
+            if (!relocationDir.mkdir()) {
+                throw new IOException("Failed to create " + config.cdcWorkingDir);
+            }
+        }
+
+        File archiveDir = new File(relocationDir, ARCHIVE_FOLDER);
+        if (!archiveDir.exists()) {
+            if (!archiveDir.mkdir()) {
+                throw new IOException("Failed to create " + archiveDir);
+            }
+        }
+        File errorDir = new File(relocationDir, ERROR_FOLDER);
+        if (!errorDir.exists()) {
+            if (!errorDir.mkdir()) {
+                throw new IOException("Failed to create " + errorDir);
+            }
+        }
+    }
+}
